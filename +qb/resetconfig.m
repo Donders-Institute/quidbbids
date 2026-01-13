817fdacb3e7b0ec0b470b8926da7588a3e17f763
Marcel Zwiers <m.zwiers@donders.ru.nl>
2026-01-13 13:13:18 +0100

    Add reset config function

---
+++ +qb/resetconfig.m
@@ -0,0 +1,12 @@
+function resetconfig()
+%RESETCONFIG Resets the config_default.json file in your home directory to factory default
+
+config_default = fullfile(char(java.lang.System.getProperty("user.home")), ".quidbbids", qb.version(), "config_default.json");
+
+disp("Resetting configuration to default: " + config_default)
+if isfile(config_default)
+    delete(config_default)
+end
+[pth, name, ext] = fileparts(config_default);
+[~,~] = mkdir(pth);
+copyfile(fullfile(fileparts(mfilename("fullpath")), name + ext), config_default)
